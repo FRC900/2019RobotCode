7bfc26b4694cb7021f5eb73f4fc587eca732fe48
E-Danforth <edward.w.danforth@gmail.com>
2019-04-04 23:01:03 +0000

    test

--- ds_stream/generate_image.py
+++ ds_stream/generate_image.py
@@ -4,8 +4,8 @@ import numpy as np
 def main():
 	imgname = "image.png"
 
-	img_height = 480
-	img_width = 640
+	img_height = 605
+	img_width = 1612
 	n_channels = 4
 	res = np.zeros((img_height, img_width, n_channels), dtype=np.uint8)
 
@@ -14,7 +14,6 @@ def main():
 	cv2.imwrite('/home/ubuntu/' + imgname, res)
 
 def drawCross(img, x, y):
-     cv2.line(img,(x,y-30),(x,y+30),(0,0,255,255),8)
-     cv2.line(img,(x-30,y),(x+30,y),(0,0,255,255),8)
+     cv2.line(img,(103,94),(1380,382),(0,0,255,255),8)
 
 main()
